7c4e61f54b486e7f2ef929c9ee82091888036532
anshubeniwal <beniwalanshum1998@gmail.com>
2020-05-26 17:33:00 +0530

    Task 2 Submitted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CustomUserPipe} from './customUserPipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomUserPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
